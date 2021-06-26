dac24b59a8c3ee17ec3312863666135e6f3658ab
carlosviana <viana.mail@gmail.com>
2021-06-26 07:51:49 -0300

    data import - ok

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,57 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     ChallengeNfl.Repo.insert!(%ChallengeNfl.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule CreatePLayers do
+  def run() do
+    "rushing.json"
+    |> read_file_and_decode()
+  end
+  def read_file_and_decode(file) do
+    file
+    |> File.read!()
+    |> Jason.decode!()
+    |> Enum.map(&create_struct/1)
+  end
+
+  def create_struct(player) do
+    %ChallengeNfl.Player{
+    name: player["Player"],
+    team: player["Team"],
+    position: player["Pos"],
+    attempts: player["Att"],
+    attempts_avg: player["Att/G"]/1,
+    total_yards: build_total_yards_field(player["Yds"]),
+    avg_yards_per_attempts: player["Avg"]/1,
+    yards_per_game: player["Yds/G"]/1,
+    total_touchdowns: player["TD"],
+    longest_rush: build_longest_rush(player["Lng"]),
+    longest_rush_with_touchdown: build_longest_rush_with_touch_down(player["Lng"]),
+    first_down: player["1st"],
+    first_down_ratio: player["1st%"]/1,
+    fumbles: player["FUM"],
+    twenty_yards: player["20+"],
+    forty_yards: player["40+"]
+    }
+    |> ChallengeNfl.Repo.insert!
+  end
+  defp build_total_yards_field(total_yards) do
+    ( total_yards |> is_integer() && total_yards ) ||
+    total_yards
+    |> String.replace(",", "")
+    |> String.to_integer()
+  end
+
+  defp build_longest_rush(lng) do
+    ( lng |> is_integer() && lng ) ||
+    lng
+    |> String.replace("T", "")
+    |> String.to_integer()
+  end
+
+  defp build_longest_rush_with_touch_down(lng) do
+    if is_integer(lng) do
+      false
+    else
+      String.contains?(lng, "T")
+    end
+  end
+end
+
+CreatePLayers.run()
